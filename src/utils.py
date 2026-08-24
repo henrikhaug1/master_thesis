133625c53c92431244af57f47acc94b1e6c82fc6
Henrik Haug <henrik.h.haug@gmail.com>
2026-08-24 11:17:39 +0200

    Added a partials function for calculating derivatives in multivariable functions

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,30 @@
-# src/utils.py
-import jax, jax.numpy as jnp
+import jax
+import jax.numpy as jnp
 
 
-def derivatives(model, t, order=2):
-    def u_scalar(ti):
-        return model(jnp.reshape(ti, (1, 1)))[0, 0]
+def derivatives(model, x, order=2):
+    def u_scalar(xi):
+        return model(jnp.reshape(xi, (1, 1)))[0, 0]
 
     fns = [u_scalar]
     for _ in range(order):
         fns.append(jax.grad(fns[-1]))
-    return [jax.vmap(f)(t) for f in fns]
+    return [jax.vmap(f)(x) for f in fns]
+
+
+def partials(model, x, t):
+    def u_scalar(xi, ti):
+        return model(jnp.stack([xi, ti]).reshape(1, 2))[0, 0]
+
+    du_x = jax.grad(u_scalar, argnums=0)
+    du_t = jax.grad(u_scalar, argnums=1)
+    du_xx = jax.grad(du_x, argnums=0)
+    du_tt = jax.grad(du_t, argnums=1)
+    v = jax.vmap
+    return (
+        v(u_scalar)(x, t),
+        v(du_x)(x, t),
+        v(du_t)(x, t),
+        v(du_xx)(x, t),
+        v(du_tt)(x, t),
+    )

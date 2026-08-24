1adaf9c982ed250fd520d7e873c4c965654c700f
Henrik Haug <henrik.h.haug@gmail.com>
2026-08-24 11:01:13 +0200

    Implemented use of bcs in loss and edited logic slightly.

--- src/loss.py
+++ src/loss.py
@@ -1,6 +1,13 @@
 import jax.numpy as jnp
 
 
-def loss_fn(model, t_collocation, residual, ic_fn):
+def loss_fn(model, t_collocation, residual, ic_fn=None, bc_fn=None):
     pde_loss = jnp.mean(residual(model, t_collocation) ** 2)
-    return pde_loss + ic_fn(model)
+    loss = pde_loss
+    if ic_fn is None and bc_fn is None:
+        raise ValueError("Define boundary- or initial conditions")
+    if ic_fn is not None:
+        loss += ic_fn(model)
+    if bc_fn is not None:
+        loss += bc_fn(model)
+    return loss

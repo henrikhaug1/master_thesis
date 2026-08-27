59540600cf68cc852be643cb5206173f302e0efb
Henrik Haug <henrik.h.haug@gmail.com>
2026-08-27 11:32:27 +0200

    Adding typehinting and max_error now returning float

--- src/metrics.py
+++ src/metrics.py
@@ -1,13 +1,22 @@
+import jax
 import jax.numpy as jnp
+import flax.nnx as nnx
+from jax import Array
 
 
-def l2_error(u_pred, u_exact):
+def l2_error(u_pred: Array, u_exact: Array) -> float:
     return float(jnp.sqrt(jnp.mean((u_pred - u_exact) ** 2)))
 
 
-def rel_l2_error(u_pred, u_exact):
+def rel_l2_error(u_pred: Array, u_exact: Array) -> float:
     return float(jnp.linalg.norm(u_pred - u_exact) / jnp.linalg.norm(u_exact))
 
 
-def max_error(u_pred, u_exact):
-    return jnp.max(jnp.abs(u_pred - u_exact))
+def max_error(u_pred: Array, u_exact: Array) -> float:
+    return float(jnp.max(jnp.abs(u_pred - u_exact)))
+
+
+def n_params(model: nnx.Module) -> int:
+    return sum(
+        a.size for a in jax.tree.leaves(nnx.to_pure_dict(nnx.state(model, nnx.Param)))
+    )
